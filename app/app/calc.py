3e6f2a929c9b968bbf7ef30ee067ef1665ca87fc
dmalinovschi <dmitrii.malinovschii@gmail.com>
2019-07-05 17:48:39 +0300

    - added simple sanity test

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,3 +3,7 @@
 def sum(a, b):
     """Add two nubers"""
     return a + b
+
+def subtract(a, b):
+    """Subtract two nubers"""
+    return a - b
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from app.calc import sum
+from app.calc import sum, subtract
 
 
 class CalcTests(TestCase):
@@ -8,3 +8,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Two numbers are summed properly"""
         self.assertEqual(sum(3, 8), 11)
+
+    def test_subtract_test(self):
+        """Two numbers are subtracted properly"""
+        self.assertEqual(subtract(10,5),5)
