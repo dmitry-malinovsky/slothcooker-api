a2bf893a12aedf7829a8d0df2b7b45ea4665a57c
dmalinovschi <dmitrii.malinovschii@gmail.com>
2019-07-09 16:47:04 +0300

    - added user change page test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -24,3 +24,9 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
