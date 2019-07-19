76d4fd0a68d67a9db35b885d53b6aa7883346ad4
dmalinovschi <dmitrii.malinovschii@gmail.com>
2019-07-19 12:21:06 +0300

    - syntactical fix

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,14 +8,14 @@ from django.test import TestCase
 class CommandTests(TestCase):
 
     def test_wait_for_db_ready(self):
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
-            get_item.return_value = True
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
             call_command('wait_for_db')
-            self.assertEqual(get_item.call_count, 1)
+            self.assertEqual(gi.call_count, 1)
 
     @patch('time.sleep', return_value=True)
     def test_wait_for_db(self, time_sleep):
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
-            get_item.side_effect = [OperationalError] * 5 + [True]
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
-            self.assertEqual(get_item.call_count, 6)
+            self.assertEqual(gi.call_count, 6)
