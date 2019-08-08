8df9143636e2b28589f813c79b0988fb5549e85f
dmalinovschi <dmitrii.malinovschii@gmail.com>
2019-08-08 14:39:00 +0300

    - added create ingredient endpoint - added get user ingredients endpoint - added tests for ingredients

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,4 +1,4 @@
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from rest_framework import serializers
 
@@ -6,5 +6,12 @@ from rest_framework import serializers
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
